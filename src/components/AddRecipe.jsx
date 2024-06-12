cefe1603dc10f7ee4952994afe8068f8a1a1c2cc
meghana-wq <meghanakh000@gmail.com>
2024-06-13 00:45:57 +0530

    frontend add added

--- src/components/AddRecipe.jsx
+++ src/components/AddRecipe.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const AddRecipe = () => {
   return (
-    <div>.</div>
+    <div>
+        <Navbar/>
+        <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row">
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">NAME</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">DESCRIPTION</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">RATING</label>
+                    <input type="text" className="form-control" />
+                </div>
+                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <button className="btn btn-success">ADD</button>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div></div>
   )
 }
 
